5db8a7580da1e18c2f190c04b95f26284c99d4c7
stenny453 <stennygabrielrmr@gmail.com>
2022-10-18 20:32:35 +0300

    vue component

---
+++ vue3/components/socialNetwork.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    data() {
+        return {
+            twitter: "twitter.com",
+            instagram: "instagram.com"
+        }
+    },
+}
+</script>
+
+<template>
+    <h5>{{ twitter }} - {{ instagram }}</h5>
+</template>

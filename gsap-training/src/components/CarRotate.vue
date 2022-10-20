ee5cf948d4ab9737a38bb71ba37c3aeb892b32dc
Stenny <stenny.manao@gmail.com>
2022-10-20 09:22:40 +0200

    Init Gsap animation

--- gsap-training/src/components/CarRotate.vue
+++ gsap-training/src/components/CarRotate.vue
@@ -1,5 +1,34 @@
 <template>
-    <div>
-        Hello Training
+    <div id="contain-image">
+        <h3>{{ msg }}</h3>
+        <img src="./../assets/images/4542166_W177_perf_OW_28.jpg" alt="var-illustration" class="car">
     </div>
 </template>
+
+<script>
+import { gsap } from "gsap";
+
+export default {
+    name: 'CarRotate',
+    data() {
+        return {
+            msg: "Hello training !"
+        }
+    },
+    methods: {
+        initGsap() {
+            gsap.to(".car", { duration: 3, x: 500 });
+        }
+    },  
+    mounted() {
+        this.initGsap();
+    }
+}
+</script>
+
+<style>
+    .car {
+        max-width: 100%;
+        height: 200px;
+    }
+</style>

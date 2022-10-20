caf1c94c3ea70b3c841e48c80588af654de6cdce
stenny453 <stennygabrielrmr@gmail.com>
2022-10-20 19:46:48 +0300

    gsap animation basics

--- gsap-training/src/components/CarRotate.vue
+++ gsap-training/src/components/CarRotate.vue
@@ -1,7 +1,20 @@
 <template>
     <div id="contain-image">
-        <h3>{{ msg }}</h3>
-        <img src="./../assets/images/4542166_W177_perf_OW_28.jpg" alt="var-illustration" class="car">
+        <div>
+            <h3>{{ msg }}</h3>
+            <img src="./../assets/images/4542166_W177_perf_OW_28.jpg" alt="var-illustration" class="car">
+        </div>
+        <div class="car-list">
+            <img src="./../assets/images/4542166_W177_perf_OW_28.jpg" alt="var-illustration" class="sw">
+            <img src="./../assets/images/4542166_W177_perf_OW_28.jpg" alt="var-illustration" class="sw">
+            <img src="./../assets/images/4542166_W177_perf_OW_28.jpg" alt="var-illustration" class="sw">
+        </div>
+
+        <div class="car-list">
+            <img src="./../assets/images/4542166_W177_perf_OW_28.jpg" alt="var-illustration" class="sg">
+            <img src="./../assets/images/4542166_W177_perf_OW_28.jpg" alt="var-illustration" class="sg">
+            <img src="./../assets/images/4542166_W177_perf_OW_28.jpg" alt="var-illustration" class="sg">
+        </div>
     </div>
 </template>
 
@@ -17,7 +30,42 @@ export default {
     },
     methods: {
         initGsap() {
-            gsap.to(".car", { duration: 3, x: 500 });
+            // gsap.to(".car", 
+            // { 
+            //     duration: 3, x: 500, backgroundColor: "#117f", 
+            //     border: "4px solid black", borderRadius: "5px",
+            //     ease: "elastic" 
+            // });
+
+            gsap.set(".car", { transformOrigin: "50% 50%" });
+
+            gsap.to(".car", 
+            { 
+                duration: 3,
+                rotate: 360,
+                onUpdate() {
+                    console.log("Updated");
+                },
+                onStart() {
+                    console.log("Started");
+                },
+                onComplete() {
+                    console.log("Completed");
+                }
+            });
+
+            gsap.to(".sw", 
+            { 
+                duration: 2, opacity: 0, scale: 0.3, 
+                ease: "back", stagger: 1 
+            })
+
+            gsap.from(".sg", 
+            { 
+                duration: 2, opacity: 0, 
+                stagger: 1, x: "random(-100, 100)",
+                // delay: 2
+            })
         }
     },  
     mounted() {
@@ -30,5 +78,9 @@ export default {
     .car {
         max-width: 100%;
         height: 200px;
+    }
+
+    .car-list img {
+        height: 100px;
     }
 </style>
